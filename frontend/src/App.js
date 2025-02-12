742ec83e2c4ba27f691a6831dc2890a57ec8ac78
Diego MG <diego.mg.180@outlook.com>
2025-02-12 08:51:52 +0000

    initial frontend app

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+import { jsx as _jsx } from "react/jsx-runtime";
+const App = () => {
+  return /*#__PURE__*/_jsx("div", {
+    className: "App",
+    children: /*#__PURE__*/_jsx("h1", {
+      children: "Images Gallery "
+    })
+  });
+};
+export default App;
