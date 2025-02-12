3f55cea6bf0279f69a9897671aaf5ad2590754f0
Diego MG <diego.mg.180@outlook.com>
2025-02-12 09:08:19 +0000

    initial frontend app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ const App = () => {
   return /*#__PURE__*/_jsx("div", {
     className: "App",
     children: /*#__PURE__*/_jsx("h1", {
-      children: "Images Gallery App"    })
+      children: "Images Gallery 2"    })
   });
 };
 export default App;
