6558da1109b5fecb5a8c32348697132af9e2d740
Diego MG <diego.mg.180@outlook.com>
2025-02-20 12:26:28 +0000

    First API call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,14 @@ const App = () => {
     const handleSearchSubmit = (e) => {
       e.preventDefault();
       console.log(word);
+      fetch(`https://api.unsplash.com/photos/random?query=${word}&client_id=${UNSPLASH_KEY}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
     }
 
   return (
